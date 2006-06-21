a84c832370afa146ce535c0bc2ede526f6309e18
Giovanni Putignano <gputignano@tiscali.it>
2006-06-21 08:50:00 +0000

    Installation files now are translated completely

--- it_IT/oscommerce/install/includes/languages/it_IT/index.php
+++ it_IT/oscommerce/install/includes/languages/it_IT/index.php
@@ -7,24 +7,24 @@
 #
 # Released under the GNU General Public License
 
-page_title_welcome = Welcome to osCommerce 3.0a3pre!
+page_title_welcome = Benvenuto in osCommerce 3.0a3pre!
 
-page_heading_installation_type = Installation Type
+page_heading_installation_type = Tipo di installazione
 
-title_language = Language:
+title_language = Lingua:
 
-text_welcome = <p style="background-color: #ff6633; padding: 5px; border: 1px #000 solid;">Please be aware that this is an unsuppported development alpha release that is not to be used for production use.</p><p>osCommerce is an open source e-commerce solution under on going development by the open source community. Its feature packed out-of-the-box installation allows store owners to setup, run, and maintain their online stores with minimum effort and with no costs involved.</p><p>osCommerce combines open source solutions to provide a free and open development platform, which includes the <i>powerful</i> PHP web scripting language, the <i>stable</i> Apache web server, and the <i>fast</i> MySQL database server.</p><p>With no restrictions or special requirements, osCommerce can be installed on any PHP4 or PHP5 enabled web server, on any environment that PHP and MySQL supports, which includes Linux, Solaris, BSD, and Microsoft Windows environments.</p>
-text_installation_type = Please choose the installation method to continue.
+text_welcome = <p style="background-color: #ff6633; padding: 5px; border: 1px #000 solid;">Stai usando una versione di sviluppo non supportata che non deve essere assolutamente utilizzata in ambienti di produzione.</p><p>osCommerce è una soluzione open source per l'ecommerce in continuo sviluppo grazie al contributo della comunità open source. La sua semplicità permette, a chi la utilizza, di creare e mantenere un negozio online con il minimo sforzo e con costi minimi.</p><p>Per fornire una piattaforma di sviluppo gratuita e aperta osCommerce integra diverse tecnologie open source come il <i>potente</i> linguaggio di scripting per il web PHP, lo <i>stabile</i> web server Apache e il <i>veloce</i> server database MySQL.</p><p>Senza alcuna restrizione o requisiti particolari, osCommerce può essere installato su qualsiasi web server che integri PHP4 o PHP5, su ogni ambiente che supporti sia PHP che MySQL tra i quali Linux, Solaris, BSD ed ambienti Windows.</p>
+text_installation_type = Scegli il metodo di installazione prima di continuare.
 
-box_server_title = Server Capabilities
-box_server_php_version = PHP Version
-box_server_php_settings = PHP Settings
+box_server_title = Funzionalità del Server
+box_server_php_version = Versione PHP
+box_server_php_settings = Settaggi PHP
 box_server_register_globals = register_globals
 box_server_magic_quotes = magic_quotes
 box_server_file_uploads = file_uploads
 box_server_session_auto_start = session.auto_start
 box_server_session_use_trans_sid = session.use_trans_sid
-box_server_php_extensions = PHP Extensions
+box_server_php_extensions = Estensioni di PHP
 box_server_mysql = MySQL
 box_server_gd = GD
 box_server_curl = cURL
@@ -32,8 +32,8 @@ box_server_openssl = OpenSSL
 box_server_on = On
 box_server_off = Off
 
-error_configuration_file_not_writeable = <p>The webserver does not seem to be able to write the online store parameters to its configuration file due to file permission problems.</p><p>The configuration file is located at:</p><p>%s</p>
-error_configuration_file_alternate_method = <p>Alternatively the possibility to copy the configuration parameters to the configuration file by hand is also provided at the end of the installation procedure.</p>
+error_configuration_file_not_writeable = <p>A causa di problemi relativi ai prermessi sui file è impossibile scrivere i parametri nel file di configurazione.</p><p>Il file di configurazione si trova a:</p><p>%s</p>
+error_configuration_file_alternate_method = <p>In alternativa è possibile copiare manualmente i parametri nel file di configurazione alla fine della procedura di installazione.</p>
 
 image_button_install = Install
 image_button_upgrade = Upgrade
--- it_IT/oscommerce/install/includes/languages/it_IT/install.php
+++ it_IT/oscommerce/install/includes/languages/it_IT/install.php
@@ -7,82 +7,82 @@
 #
 # Released under the GNU General Public License
 
-page_title_installation = New Installation
+page_title_installation = Nuova Installazione
 
-page_heading_step_1 = Database Server
-page_heading_step_2 = Web Server
-page_heading_step_3 = Online Store Settings
-page_heading_step_4 = Finished!
+page_heading_step_1 = Server di Database
+page_heading_step_2 = Server Web
+page_heading_step_3 = Impostazioni del Negozio Online
+page_heading_step_4 = Finito!
 
-text_installation = <p>This web-based installation routine will correctly setup and configure osCommerce to run on this server.</p><p>Please following the on-screen instructions that will take you through the database server, web server, and store configuration options. If help is needed at any stage, please consult the documentation or seek help at the community support forums.</p>
-text_successful_installation = The installation and configuration was successful!
+text_installation = <p>La routine d'installazione web configurerà osCommerce affinché possa funzionare correttamente su questo server.</p><p>Segui le istruzioni a schermo che ti guideranno durante la configurazione delle opzioni del server database, del server web e del negozio. Se hai bisogno di aiuto consulta la documentazione o chiedi aiuto nel forum di sopporto.</p>
+text_successful_installation = L'installazione e la configurazione sono state completate con successo!
 
-param_database_server = Database Server
-param_database_server_description = The address of the database server in the form of a hostname or IP address.
-param_database_username = Username
-param_database_username_description = The username used to connect to the database server.
+param_database_server = Server Database
+param_database_server_description = L'indirizzo del server database nella forma di mome host o indirizzo IP.
+param_database_username = Nome Utente
+param_database_username_description = Il Nome Utente utilizzato per la connessione al server database.
 param_database_password = Password
-param_database_password_description = The password that is used together with the username to connect to the database server.
-param_database_name = Database Name
-param_database_name_description = The name of the database to hold the data in.
-param_database_type = Database Type
-param_database_type_description = The database server software that is used.
-param_database_prefix = Database Table Prefix
-param_database_prefix_description = The prefix to use for the database tables.
+param_database_password_description = La password utilizzata in combinazione al Nome Utente per connettersi al server database.
+param_database_name = Nome del Database
+param_database_name_description = Il nome del database che conterrà i dati.
+param_database_type = Tipo di Database
+param_database_type_description = Il tipo di software utilizzato dal database.
+param_database_prefix = Prefisso per le tabelle
+param_database_prefix_description = Il prefisso utilizzato per le tabelle del database.
 
-param_database_import_sample_data = Import Sample Data
-param_database_import_sample_data_description = Inserting sample data into the database is recommended for first time installations.
+param_database_import_sample_data = Importa i dati di esempio
+param_database_import_sample_data_description = E' consigliabile importare i dati di esempio in caso di prima installazione.
 
-param_web_address = WWW Address
-param_web_address_description = The web address to the online store.
-param_web_root_directory = Webserver Root Directory
-param_web_root_directory_description = The directory where the online store is installed on the server.
-param_web_work_directory = Work Directory
-param_web_work_directory_description = The working directory for temporarily created files. This directory should be located outside the public webserver root directory for security reasons. (Shared hosting servers should not use /tmp/)
+param_web_address = Indirizzo WWW
+param_web_address_description = L'indirizzo web del negozio online.
+param_web_root_directory = Directory radice del Webserver
+param_web_root_directory_description = la directory dove è installato il negozio online sul server.
+param_web_work_directory = Directory di lavoro
+param_web_work_directory_description = Directory di lavoro per i file temporanei. Per motivi di sicurezza questa directory deve trovarsi all'esterno della directori radice del server web. (Shared hosting servers should not use /tmp/)
 
-param_store_name = Store Name
-param_store_name_description = The name of the online store that is presented to the public.
-param_store_owner_name = Store Owner Name
-param_store_owner_name_description = The name of the store owner that is presented to the public.
-param_store_owner_email_address = Store Owner E-Mail Address
-param_store_owner_email_address_description = The e-mail address of the store owner that is presented to the public.
-param_administrator_username = Administrator Username
-param_administrator_username_description = The administrator username to use for the administration tool.
-param_administrator_password = Administrator Password
-param_administrator_password_description = The password to use for the administrator account.
+param_store_name = Nome del negozio
+param_store_name_description = Nome del negozio online.
+param_store_owner_name = Nome del titolare del negozio
+param_store_owner_name_description = Il nome del titolare del negozio online.
+param_store_owner_email_address = Indirizzo e-mail del titolare del negozio
+param_store_owner_email_address_description = L'indirizzo e-mail del titolare del negozio online.
+param_administrator_username = Nome Utente Amministratore
+param_administrator_username_description = Il Nome Utente dell'Amministratore verrà usato per l'amministrazione.
+param_administrator_password = Password Administratore
+param_administrator_password_description = La password usata dall'Amministratore.
 
-box_steps_step_1 = Database Server
-box_steps_step_2 = Web Server
-box_steps_step_3 = Online Store Settings
-box_steps_step_4 = Finished!
+box_steps_step_1 = Server Database
+box_steps_step_2 = Server web
+box_steps_step_3 = Impostazione del Negozio Online
+box_steps_step_4 = Finito!
 
-box_info_step_1_title = Step 1: Database Server
-box_info_step_1_text = <p>The database server stores the content of the online store such as product information, customer information, and the orders that have been made.</p><p>Please consult your server administrator if your database server parameters are not yet known.</p>
+box_info_step_1_title = Passo 1: Server Database
+box_info_step_1_text = <p>Il database contiene le informazioni del negozio online come le informazioni sui prodotti, sui clienti e sugli ordini effettuati.</p><p>Contatta il gestore del server se non conosci i parametri di configurazione.</p>
 
-box_info_step_2_title = Step 2: Web Server
-box_info_step_2_text = <p>The web server takes care of serving the pages of the online store to the visitors and customers. The web server parameters make sure the links to the pages point to the correct location.</p><p>Temporary files such as session data and cache files are stored in the work directory. It is important that this directory is located outside the web server root directory and is protected from public access.</p>
+box_info_step_2_title = Passo 2: Server web
+box_info_step_2_text = <p>Il server web si occupa di elaborare le pagine web del negozio online che saranno visualizzate ai visitatori ed ai clienti. I parametri di configurazione sel server web fanno si che i collegamenti puntino alla posizione corretta.</p><p>I file temporanei come per es. i dati delle sessioni ed i file della cache sono memorizzati nella directory work. E' importante che questa directory sia posizionata al di fuori della directory radice del server web e che sia protetta da accessi non consentiti.</p>
 
-box_info_step_3_title = Step 3: Online Store Settings
-box_info_step_3_text = <p>Here you can define the name of your online store, and the contact information for the store owner.</p><p>The administrator username and password are used to log into the protected administration tool section.</p>
+box_info_step_3_title = Passo 3: Impostazioni del Negozio Online
+box_info_step_3_text = <p>In quest'area puoi impostare il nome del negozio online e le informazioni di contatto per il titolare del negozio.</p><p>Il nome utente e la password dell'amministratore sono utilizzate per accedere agli strumenti di amministrazione.</p>
 
-box_info_step_4_title = Step 4: Finished!
-box_info_step_4_text = <p>Congratulations on installing and configuring osCommerce as your online store solution!</p><p>We hope you all the best with your online store and welcome you to join and participate in our community.</p><p align="right">- The osCommerce Team</p>
+box_info_step_4_title = Passo 4: Finito!
+box_info_step_4_text = <p>Congratulazioni per aver usato osCommerce come tua soluzione ecommerce per il tuo negozio online!</p><p>Ti auguriamo buona fortuna con il tuo nuovo negozio online e ti inviatiamo ad unirti e partecipare alla nostra community.</p><p align="right">- Il team di osCommerce</p>
 
-error_configuration_file_not_writeable = <p>The webserver was not able to write the online store parameters to its configuration file due to file permission problems.</p><p>Please verify the permissions of the configuration file to allow the webserver to write to it, and try again by clicking on the Retry button below.</p><p>The configuration file is located at:</p><p>%s</p>
-error_configuration_file_alternate_method = <p>Alternatively you can copy and save the contents of the textbox below to the configuration file by hand.</p>
+error_configuration_file_not_writeable = <p>Il server web non ha potuto scrivere i parametri di configurazione del negozio online nel file di configurazione a causa di problemi con i permessi di accesso al file.</p><p>Per favore verifica i permessi del file di configurazione affinché il server web possa modificarlo, e riprova un'altra volta cliccando sul sottostante pulsante Riprova.</p><p>Il file di configurazione si trova in:</p><p>%s</p>
+error_configuration_file_alternate_method = <p>In alternativa puoi copiare manualmente ed in seguito salvare il contenuto della casella di testo sottostante nel file di configurazione.</p>
 
-rpc_database_connection_test = Testing database connection..
-rpc_database_connection_error = There was a problem connecting to the database server. The following error had occured:</p><p><b>%s</b></p><p>Please verify the connection parameters and try again.
-rpc_database_connected = Successfully connected to the database.
-rpc_database_importing = The database structure is now being imported. Please be patient during this procedure.
-rpc_database_imported = Database imported successfully.
-rpc_database_import_error = There was a problem importing the database. The following error had occured:</p><p><b>%s</b></p><p>Please verify the connection parameters and try again.
+rpc_database_connection_test = Sto testando la connessione con il database..
+rpc_database_connection_error = Si è verificato un problema nel tentativo di connessione al server database. L'errore che si è verificato è:</p><p><b>%s</b></p><p>Verifica i parametri di connessione e riprova.
+rpc_database_connected = Sei connesso al database.
+rpc_database_importing = Ora sarà importata la struttura del database. Abbi pazienza durante questa fase.
+rpc_database_imported = Il database è stato importato con successo.
+rpc_database_import_error = Si è verificato un problema nel tentativo di importare il database. L'errore che si è verificato è:</p><p><b>%s</b></p><p>Verifica i parametri di connessione e riprova.
 
-rpc_work_directory_test = Testing work directory..
-rpc_work_directory_error_non_existent = There was a problem accessing the working directory. The following error had occured:<br /><br /><b>The directory does not exist:<br /><br />%s</b><br /><br />Please verify the directory and try again.
-rpc_work_directory_error_not_writeable = There was a problem accessing the working directory. The following error had occured:<br /><br /><b>The webserver does not have write permissions to the directory:<br /><br />%s</b><br /><br />Please verify the permissions of the directory and try again.
-rpc_work_directory_configured = Working directory successfully configured.
+rpc_work_directory_test = Sto testando la directory di lavoro..
+rpc_work_directory_error_non_existent = Si è verificato un problema nel tentativo di accedere la directory di lavoro. L'errore che si è verificato è:<br /><br /><b>La directory non esiste:<br /><br />%s</b><br /><br />Verifica i parametri di connessione e riprova.
+rpc_work_directory_error_not_writeable = Si è verificato un problema nel tentativo di accedere la directory di lavoro. L'errore che si è verificato è:<br /><br /><b>Il server web non ha i permessi per scrivere la directory:<br /><br />%s</b><br /><br />Verifica i permessi per scrivere la directory e riprova.
+rpc_work_directory_configured = La directory di lavoro è stata configurata con successo.
 
-rpc_database_sample_data_importing = The sample data is now being imported into the database. Please be patient during this procedure.
-rpc_database_sample_data_imported = Database sample data imported successfully.
-rpc_database_sample_data_import_error = There was a problem importing the database sample data. The following error had occured:</p><p><b>%s</b></p><p>Please verify the database server and try again.
+rpc_database_sample_data_importing = Ora saranno importati i dati di esempio. Abbi pazienza durante questa fase.
+rpc_database_sample_data_imported = Il database di esempio è stato importato con successo.
+rpc_database_sample_data_import_error = Si è verificato un problema nel tentativo di importare il database di esempio. L'errore che si è verificato è:</p><p><b>%s</b></p><p>Verifica il server database e riprova.
