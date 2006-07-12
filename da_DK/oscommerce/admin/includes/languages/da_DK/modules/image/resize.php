44add552064f34bd9b5b8a610ff4bbba33d31516
Harald Ponce de Leon <hpdl@oscommerce.com>
2006-07-12 10:07:35 +0000

    setup files for Danish translations

---
+++ da_DK/oscommerce/admin/includes/languages/da_DK/modules/image/resize.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id: $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2006 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_IMAGE_RESIZE', 'Resize Images');
+
+define('MODULE_IMAGE_RESIZE_GROUPS', 'Groups');
+define('MODULE_IMAGE_RESIZE_NUMBER_OF_IMAGES', 'Number of Images Resized');
+
+define('MODULE_IMAGE_RESIZE_PROCESS_GROUPS', 'Groups');
+define('MODULE_IMAGE_RESIZE_OVERWRITE_IMAGES', 'Overwrite Existing Images');
+?>
