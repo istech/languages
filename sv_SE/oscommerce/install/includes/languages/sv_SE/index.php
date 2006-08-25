354ebc2123bb20ef9205a30f454ad2471c727b47
Anders Pamdal <anders@pamdal.se>
2006-08-25 21:11:45 +0000

    This should complete the swedish translationfiles, any feedback would be nice... To help sort out the bad spelling and find buggs.

--- sv_SE/oscommerce/install/includes/languages/sv_SE/index.php
+++ sv_SE/oscommerce/install/includes/languages/sv_SE/index.php
@@ -7,14 +7,14 @@
 #
 # Released under the GNU General Public License
 
-page_title_welcome = Welcome to osCommerce 3.0a3pre!
+page_title_welcome = Välkommen till osCommerce 3.0a3pre!
 
 page_heading_installation_type = Installationstyp
 
 title_language = Språk:
 
-text_welcome = <p style="background-color: #ff6633; padding: 5px; border: 1px #000 solid;">Please be aware that this is an unsuppported development alpha release that is not to be used for production use.</p><p>osCommerce is an open source e-commerce solution under on going development by the open source community. Its feature packed out-of-the-box installation allows store owners to setup, run, and maintain their online stores with minimum effort and with no costs involved.</p><p>osCommerce combines open source solutions to provide a free and open development platform, which includes the <i>powerful</i> PHP web scripting language, the <i>stable</i> Apache web server, and the <i>fast</i> MySQL database server.</p><p>With no restrictions or special requirements, osCommerce can be installed on any PHP4 or PHP5 enabled web server, on any environment that PHP and MySQL supports, which includes Linux, Solaris, BSD, and Microsoft Windows environments.</p>
-text_installation_type = Please choose the installation method to continue.
+text_welcome = <p style="background-color: #ff6633; padding: 5px; border: 1px #000 solid;">Var vänlig och inse att detta är en utvecklingsversion som ej är avsedd för produktionssyfte.</p><p>osCommerce är en open source e-handelslösning som utvecklas under open source community. Dess funktionsfyllda ur-lådan installation tillåter affärsinnehavaren att installera, köra, och underhålla deras onlineaffärer med minsta möjliga ansträngning och till lägsta kostnad.</p><p>osCommerce kombinerar open source lösning att förse en gratis och öppen utvecklingsplattform, vilket innehåller ett <i>kraftfullt</i> PHP webscriptspråk, den <i>stabila</i> Apache webservern, och den <i>snabba</i> MySQL databasservern.</p><p>Utan restriktioner eller speciella krav, osCommerce kan bli installerat på vilken PHP4 eller PHP5 webserver som helst, och vilken miljö som helst som stöder PHP och MySQL, vilket innefattar Linux, Solaris, BSD, och Microsoft Windows miljöer.</p>
+text_installation_type = Var vänlig och välj installationsmetod för att fortsätta.
 
 box_server_title = Server kapacitet
 box_server_php_version = PHP version
@@ -32,8 +32,8 @@ box_server_openssl = OpenSSL
 box_server_on = Av
 box_server_off = På
 
-error_configuration_file_not_writeable = <p>The webserver does not seem to be able to write the online store parameters to its configuration file due to file permission problems.</p><p>The configuration file is located at:</p><p>%s</p>
-error_configuration_file_alternate_method = <p>Alternatively the possibility to copy the configuration parameters to the configuration file by hand is also provided at the end of the installation procedure.</p>
+error_configuration_file_not_writeable = <p>Webservern verkar inte kunna skriva onlineaffärens parametrar till konfigurationsfilen på grund av skrivrättighetsproblem.</p><p>Konfigurationsfiles hittas vid:</p><p>%s</p>
+error_configuration_file_alternate_method = <p>Alternativt kan du kopiera in parametrarna manuellt till konfigurationsfilen som presenteras i slutet av installationsprocessen.</p>
 
 image_button_install = Installera
 image_button_upgrade = Upgradera
--- sv_SE/oscommerce/install/includes/languages/sv_SE/install.php
+++ sv_SE/oscommerce/install/includes/languages/sv_SE/install.php
@@ -31,12 +31,12 @@ param_database_prefix = Database table prefix
 param_database_prefix_description = Prefix att använda till databasens tables.
 
 param_database_import_sample_data = Importera Exempeldata
-param_database_import_sample_data_description = Inserting sample data into the database is recommended for first time installations.
+param_database_import_sample_data_description = Importera exempeldata till databasen är att rekommendera för förstagångs installationer.
 
 param_web_address = WWW Adress
 param_web_address_description = Webadressen till onlineaffären.
-param_web_root_directory = Webserver Root Directory
-param_web_root_directory_description = The directory where the online store is installed on the server.
+param_web_root_directory = Webserver root katalog
+param_web_root_directory_description = Katalogen där onlineaffären är installerad på denna server.
 param_web_work_directory = Arbetskatalog
 param_web_work_directory_description = Arbetskatalogen för temporärt skapade filer. Denna katalog bör ligga utanför webserverns rootkatalog av säkerhetsskäl. (Delade hosting servrar bör ej använda /tmp/)
 
@@ -51,38 +51,38 @@ param_administrator_username_description = Administratörens användarnamn för 
 param_administrator_password = Administratörens lösenord
 param_administrator_password_description = Lösenordet för att logga in som administratör.
 
-box_steps_step_1 = Databas server
-box_steps_step_2 = Web server
+box_steps_step_1 = Databasserver
+box_steps_step_2 = Webserver
 box_steps_step_3 = Onlineaffärens inställningar
 box_steps_step_4 = Klar!
 
 box_info_step_1_title = Steg 1: Databasserver
-box_info_step_1_text = <p>The database server stores the content of the online store such as product information, customer information, and the orders that have been made.</p><p>Please consult your server administrator if your database server parameters are not yet known.</p>
+box_info_step_1_text = <p>Databasservern sparar innehållet från onlineaffären som produktinformation, kundinformation, och ordrar som gjorts.</p><p>Var vänlig och konsultera din serveradministratör om dina databasserverparametrar ej är kända.</p>
 
 box_info_step_2_title = Steg 2: Webserver
-box_info_step_2_text = <p>The web server takes care of serving the pages of the online store to the visitors and customers. The web server parameters make sure the links to the pages point to the correct location.</p><p>Temporary files such as session data and cache files are stored in the work directory. It is important that this directory is located outside the web server root directory and is protected from public access.</p>
+box_info_step_2_text = <p>Webservern tar hand om att visa sidor från onlineaffären till besökare och kunder. Webserverparameterarna garanterar att länkarna till sidorna pekar till rätt ställe.</p><p>Temporära filer som sessionsdata och cachefiler sparas i arbetskatalogen. Det är viktigt att denna katalog befinner sig utanför webserverns rootkatalog och skyddad från allmän tillgång.</p>
 
 box_info_step_3_title = Steg 3: Online Affärsinställningar
-box_info_step_3_text = <p>Here you can define the name of your online store, and the contact information for the store owner.</p><p>The administrator username and password are used to log into the protected administration tool section.</p>
+box_info_step_3_text = <p>Här kan du definiera namnet på din onlineaffär, och kontaktinformation till affärsinnehavaren.</p><p>Administratör användarnamn och lösenord används för att skydda administrationsverktygsdelen.</p>
 
 box_info_step_4_title = Steg 4: Klar!
-box_info_step_4_text = <p>Congratulations on installing and configuring osCommerce as your online store solution!</p><p>We hope you all the best with your online store and welcome you to join and participate in our community.</p><p align="right">- The osCommerce Team</p>
+box_info_step_4_text = <p>Gratulerar till din nya installation och konfiguration av osCommerce som din onlineaffärslösning!</p><p>Vi önskar dig all lycka med din onlineaffär och välkomnar dig att delta i vårt community.</p><p align="right">- osCommerce gruppen</p>
 
-error_configuration_file_not_writeable = <p>The webserver was not able to write the online store parameters to its configuration file due to file permission problems.</p><p>Please verify the permissions of the configuration file to allow the webserver to write to it, and try again by clicking on the Retry button below.</p><p>The configuration file is located at:</p><p>%s</p>
-error_configuration_file_alternate_method = <p>Alternatively you can copy and save the contents of the textbox below to the configuration file by hand.</p>
+error_configuration_file_not_writeable = <p>Webservern kunde ej skriva onlineaffärens parametrar till konfigurationsfilen på grund av skrivrättighetsproblem.</p><p>Var vänlig och bekräfta rättigheterna i konfigurationsfilen för att låta webservern skriva till den, och försök igen genom att klicka på försök igen knappen under.</p><p>Konfigurationsfilen finns vid:</p><p>%s</p>
+error_configuration_file_alternate_method = <p>Alternativt kan du kopiera och spara innehållet från textboxen under till konfigurationsfilen för hand.</p>
 
-rpc_database_connection_test = Testing database connection..
-rpc_database_connection_error = There was a problem connecting to the database server. The following error had occured:</p><p><b>%s</b></p><p>Please verify the connection parameters and try again.
-rpc_database_connected = Successfully connected to the database.
-rpc_database_importing = The database structure is now being imported. Please be patient during this procedure.
-rpc_database_imported = Database imported successfully.
-rpc_database_import_error = There was a problem importing the database. The following error had occured:</p><p><b>%s</b></p><p>Please verify the connection parameters and try again.
+rpc_database_connection_test = Testar databasens anslutning..
+rpc_database_connection_error = Det uppstod ett problem vid anslutningen till databasservern. Följande fel uppstod:</p><p><b>%s</b></p><p>Var vänlig och bekräfta anslutningensparametrarna och försök igen.
+rpc_database_connected = Lyckad anslutning till databasen.
+rpc_database_importing = Databasstrukturen importeras nu. Var vänlig och vänta ut processen.
+rpc_database_imported = Databasen importerad.
+rpc_database_import_error = Det uppstod ett problem vid importeringen av databasen. Följande fel uppstod:</p><p><b>%s</b></p><p>Var vänlig och bekräfta anslutningsparametrarna och försök igen.
 
-rpc_work_directory_test = Testing work directory..
-rpc_work_directory_error_non_existent = There was a problem accessing the working directory. The following error had occured:<br /><br /><b>The directory does not exist:<br /><br />%s</b><br /><br />Please verify the directory and try again.
-rpc_work_directory_error_not_writeable = There was a problem accessing the working directory. The following error had occured:<br /><br /><b>The webserver does not have write permissions to the directory:<br /><br />%s</b><br /><br />Please verify the permissions of the directory and try again.
-rpc_work_directory_configured = Working directory successfully configured.
+rpc_work_directory_test = Testar arbetskatalogen..
+rpc_work_directory_error_non_existent = Det uppstod ett problem med arbetskatalogen. Följande fel uppstod:<br /><br /><b>Katalogen existerar ej:<br /><br />%s</b><br /><br />Var vänlig och bekräfta katalogen och försök igen.
+rpc_work_directory_error_not_writeable = Det uppstod ett problem med arbetskatalogen. Följande fel uppstod:<br /><br /><b>Webservern har ej skrivrättigheter till katalogen:<br /><br />%s</b><br /><br />Var vänlig och bekräfta rättigheterna och försök igen.
+rpc_work_directory_configured = Arbetskatalogen konfigurerad.
 
-rpc_database_sample_data_importing = The sample data is now being imported into the database. Please be patient during this procedure.
-rpc_database_sample_data_imported = Database sample data imported successfully.
-rpc_database_sample_data_import_error = There was a problem importing the database sample data. The following error had occured:</p><p><b>%s</b></p><p>Please verify the database server and try again.
+rpc_database_sample_data_importing = Exempeldata importeras nu till databasen. Var vänlig och vänta ut processen.
+rpc_database_sample_data_imported = Exempeldata är nu importerade i databasen.
+rpc_database_sample_data_import_error = Det uppstod ett problem vid importeringen av exempeldatan. Följande fel uppstod:</p><p><b>%s</b></p><p>Var vänlig och bekräfta databasservern och försök igen.
